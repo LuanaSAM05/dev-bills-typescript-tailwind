68bbd39c47ef27f2eb0bffa826150ef480b1e42a
Luana Melo <lsilvaalvesmelo@gmail.com>
2026-06-20 19:57:05 +0100

    fix: limitando memoria do tsc para o Render

--- backend/dist/routes/category.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const category_controller_js_1 = require("../controllers/categories/category.controller.js");
-const auth_middlewares_js_1 = require("../middlewares/auth.middlewares.js");
-const categoryRoutes = async (fastify) => {
-    fastify.addHook("preHandler", auth_middlewares_js_1.authMiddleware);
-    fastify.get("/", category_controller_js_1.getCategories);
-};
-exports.default = categoryRoutes;
